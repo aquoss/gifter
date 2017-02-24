c35fbf669f794edea8ad5be2c57ece8e8f820787
aquoss <amberquoss@gmail.com>
2017-02-24 12:15:42 -0800

    creates recipient and event data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.destroy_all
+# Recipient.destroy_all
+# Event.destroy_all
+# RecipientEvent.destroy_all
+
+# 
+# Recipient.create(first_name: "Pam", last_name: "Quoss", address_line_1: "3448 Old Military Rd", address_line_2: "", zip_code: "97502", city: "Central Point", state: "OR", country: "USA", user: User.first)
+#
+# event = [Event.create(calendar_date: 3.days.from_now.to_date, occasion: "Birthday", recurring_yearly: true),
+# Event.create(calendar_date: 20.days.from_now.to_date, occasion: "Mother's Day", recurring_yearly: true),
+# Event.create(calendar_date: 50.days.from_now.to_date, occasion: "Graduation", recurring_yearly: false)]
+
+recipientEvent = [RecipientEvent.create(recipient_id:1, event_id:11),
+RecipientEvent.create(recipient_id:1, event_id:10),
+RecipientEvent.create(recipient_id:1, event_id:12)]
