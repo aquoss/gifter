0db92a3b87a29374d12de466c60d5e3d610e479e
aquoss <amberquoss@gmail.com>
2017-02-24 19:12:21 -0800

    creates gift recommendation method

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -1,7 +1,18 @@
 class GiftsController < ApplicationController
 
   def getRecommendations
-    
+    recipient = find_by_id(params[:id])
+    matches = {}
+    Gifts.all.each do |gift|
+      count = 0
+      gift.traits.each do |key, value|
+        if recipient.traits.key == value
+          count ++
+        end
+        matches.gift = count
+      end
+    end
+    matches.sort_by{|x,y| y}.to_h.reverse
   end
 
 end
