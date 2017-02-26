3e4bb3ea06a81f0107ce48021deeb74117b77804
aquoss <amberquoss@gmail.com>
2017-02-26 13:08:37 -0800

    fixes sorting method logic

--- app/helpers/recipients_helper.rb
+++ app/helpers/recipients_helper.rb
@@ -1,7 +1,14 @@
 module RecipientsHelper
 
-  def sortEvents events
-    events.sort_by &:calendar_date 
+  def sortUpcomingEvents events
+    upcoming = events.where("calendar_date > ?", Date.today)
+    upcoming.sort_by &:calendar_date
+  end
+
+  def sortPastEvents events
+    past = events.where("calendar_date < ?", Date.today)
+    sorted = past.sort_by &:calendar_date
+    sorted.reverse
   end
 
 end
