e3ea21a2be26471e1d425308a256f4d77017aa6c
aquoss <amberquoss@gmail.com>
2017-02-26 17:45:14 -0800

    fixes user update parameters through devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,12 +12,14 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(user)
     user_path(user)
   end
-  
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:
       [:first_name, :last_name, :phone_number, :phone_carrier, :email_reminder, :sms_reminder,  :address_line_1, :address_line_2, :zip_code, :city, :state, :country])
+    devise_parameter_sanitizer.permit(:account_update, keys:
+      [:phone_number, :phone_carrier, :email_reminder, :sms_reminder,  :address_line_1, :address_line_2, :zip_code, :city, :state, :country])
   end
 end
 
