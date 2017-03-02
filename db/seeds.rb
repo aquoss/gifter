08f2eff3d40f5cd0e7a5792bc24f3c6f63d3f64b
aquoss <amberquoss@gmail.com>
2017-03-01 19:06:51 -0800

    adds images to gifts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 # Event.destroy_all
 # Gift.destroy_all
 
-#
+
 # recipient = Recipient.create(first_name: "Pam", last_name: "Quoss", address_line_1: "3448 Old Military Rd", address_line_2: "", zip_code: "97502", city: "Central Point", state: "OR", country: "USA", user: User.first)
-#
+
 # events = [Event.create(calendar_date: 3.days.from_now.to_date, occasion: "Birthday", recurring_yearly: true),
 # Event.create(calendar_date: 20.days.from_now.to_date, occasion: "Mother's Day", recurring_yearly: true),
 # Event.create(calendar_date: 50.days.from_now.to_date, occasion: "Graduation", recurring_yearly: false)]
@@ -19,8 +19,8 @@
 # Gift.create(item:"Movie Ticket", price:12.99)
 
 # Order.create(event_id:4, gift_id:Gift.first.id, tracking_number:"fdh23748o234", confirmed_delivery:false)
-#
-# Trait.create(reading:true, film:true, cooking:true, art:true, electronics:false, sports:false, fashion:true, gaming:false, music:true, travel:true, outdoor_activities:false, age:"adult", female:true, male:false, cheapest_price:false, low_price:false, med_price:true, high_price:true, traitable_type:Recipient, traitable_id:2)
+
+# Trait.create(reading:true, film:true, cooking:true, art:true, electronics:false, sports:false, fashion:true, gaming:false, music:true, travel:true, outdoor_activities:false, age:"adult", female:true, male:false, cheapest_price:false, low_price:false, med_price:true, high_price:true, traitable_type:"Recipient", traitable_id:1)
 
 gift_data = []
 40.times do
@@ -52,7 +52,7 @@ trait_data = []
     low_price: FFaker::Boolean.random,
     med_price: FFaker::Boolean.random,
     high_price: FFaker::Boolean.random,
-    traitable_type: "Gift",
+    traitable_type: Gift,
     traitable_id: num
   }
 end
